2ac7e5f06ca3fd561e520a83fe47f57a49f448c5
Haryo30 <hrydwntr22@gmail.com>
2023-01-23 17:38:47 +0700

    navbar & carosel

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes } from 'react-router-dom'
 import Text from '../components/Text'
+import Carosel from '../components/Carosel'
 
 const HomePage = () => {
   return (
     <div className='row my-3 text-left'>
-      <h6>Welcome To Inventory Apps</h6>
+      <Carosel></Carosel>
       <h3>DASHBOAD</h3>
+      
       <Text></Text>
       <div>
       <img src='http://tk.budyawacana.sch.id/joimg/publikasi/680999default-image.jpg'></img>
